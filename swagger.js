9fdaa0e25324d4d51e0b96a216ca76086fc9d100
kellyannclark <thefaithseed@gmail.com>
2023-10-04 14:45:03 -0600

    fixed all localhost 3000

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: "My Api",
         description: "Gospel Books Api"
     },
-    host: "localhost: 3000",
+    host: "localhost:3000",
     schemes: ["https", "http"]
 };
 
