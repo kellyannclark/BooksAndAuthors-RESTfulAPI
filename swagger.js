5f48e88453fb599b6969bf9e3e422abfaa24f695
kellyannclark <thefaithseed@gmail.com>
2023-10-04 13:44:58 -0600

    install body parser

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const doc = {
 };
 
 const outputFile = "./swagger.json";
-const endpointsFiles = ["./routes/index.js"];
+const endpointsFiles = ["./routes/users.js"];
 
 //this will generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
